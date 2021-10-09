4accdd02b9421a212b6d1fc60d55aac87e49b095
anvlad25 <82762246+anvlad25@users.noreply.github.com>
2021-10-09 12:22:25 +0300

    anim

--- app/src/main/java/com/example/nasapic/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/nasapic/ui/main/MainFragment.kt
@@ -9,18 +9,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import coil.api.load
-import com.example.nasapic.databinding.MainFragmentBinding
+import com.example.nasapic.databinding.MainFragmentStartBinding
 import com.example.nasapic.model.json_model.pic_of_the_day.PictureData
 
 class MainFragment : Fragment() {
-    private var _binding: MainFragmentBinding? = null
+    private var _binding: MainFragmentStartBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        _binding = MainFragmentStartBinding.inflate(inflater, container, false)
         return binding.root
     }
 
